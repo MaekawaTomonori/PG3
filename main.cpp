83fe882c4d858818400573454e4a97bb9b9b4add
Apocalypse <k023g0063@g.neec.ac.jp>
2024-10-08 12:07:52 +0900

    RecursiveFunc

--- main.cpp
+++ main.cpp
@@ -1,7 +1,40 @@
-#include <cstdio>
+#include <iostream>
+
+
+template <typename T>
+T Calculation(T x, T y) {
+    return static_cast<T>(x + y);
+}
+
+int NormalWages(int hours) {
+    return 1072 * hours;
+}
+
+int RecursiveWages(int hours) {
+    if(hours == 1){
+        return 100;
+    }
+
+    return RecursiveWages(hours-1) * 2 - 50;
+}
 
 int main() {
-    printf("こんにちは, World!\n");
+	int hours = 1;
 
-    return 0;
+    int n = NormalWages(hours);
+	int r = RecursiveWages(hours);
+
+    
+    while(r < n){
+
+        n = NormalWages(hours);
+        r = RecursiveWages(hours);
+
+
+        std::cout << hours << " : " << n << " / " << r << std::endl;
+        ++hours;
+    }
+
+
+	return 0;
 }
